efa76ae3ae3578f4d35ff45e1ee7ec363067f008
weipengtest <1009961778@qq.com>
2018-10-18 14:54:58 +0800

    update TestA

--- src/git/test/TestA.java
+++ src/git/test/TestA.java
@@ -2,12 +2,20 @@ package git.test;
 
 public class TestA {
 	private String a;
-
+	private String b;
 	public String getA() {
 		return a;
 	}
 
 	public void setA(String a) {
 		this.a = a;
+	}
+
+	public String getB() {
+		return b;
+	}
+
+	public void setB(String b) {
+		this.b = b;
 	}
 }
